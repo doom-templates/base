75183ecf03e0e6c7ba07466b02da0dc45f08c6bf
cecil-su <cecil_su@163.com>
2017-09-19 10:44:45 +0800

    add meta.js

---
+++ meta.js
@@ -0,0 +1,24 @@
+module.exports = {
+  "helpers": {
+    "if_or": function (v1, v2, options) {
+      if (v1 || v2) {
+        return options.fn(this)
+      }
+
+      return options.inverse(this)
+    }
+  },
+  "prompts": {
+    "name": {
+      "type": "string",
+      "required": true,
+      "message": "Project name"
+    },
+    "description": {
+      "type": "string",
+      "required": false,
+      "message": "Project description",
+      "default": "A base project"
+    }
+  }
+}
